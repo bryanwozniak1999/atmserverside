39965a44a6e2ae80ca1e65b52289b5170764f4ca
Olenny Polanco <opolanco1@fordham.edu>
2020-04-03 01:27:49 -0400

    Added deposit recognition

--- src/ServerUtils/sockServer.java
+++ src/ServerUtils/sockServer.java
@@ -224,6 +224,14 @@ public class sockServer implements Runnable
                             bankAccounts.put(args[2], new BankAccount(args[0], args[1], args[2]));
                         }
                     }
+                    else if (clientString.contains("Deposit>")) {
+                    	String tokens[] = clientString.split("\\>");
+                        String args[] = tokens[2].split("\\,");
+
+                        if (!bankAccounts.containsKey(args[2])) {
+                            transLog.wrBankTransactionData(String.join(",", args));
+                        }
+                    }
                     else if (clientString.contains("Date>"))
                     {
                         numOfMessages++;
